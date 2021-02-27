fbbc750dd9fca9ab39b3f797fa0b59ea81d43886
Leonardo Rivera <leodrivera@gmail.com>
2021-02-27 12:16:48 -0300

    Inclusão de novas das tabelas

--- Exercicio_23.02.py
+++ Exercicio_23.02.py
@@ -9,42 +9,83 @@
 
 import os, sys, re, sqlite3
 
-class Pessoa:
+class Person:
     def __init__(self):
         self.first_name = input("Por favor, insira o *primeiro nome* da pessoa:\n")
         self.last_name = input("Por favor, insira o *último nome* da pessoa:\n")
         self.birth = input("Por favor, insira a *data de nascimento* da pessoa:\n")
         self.email = input("Por favor, insira o *e-mail* da pessoa:\n")
+        cursor.execute("INSERT INTO Person (first_name,last_name,email,birth) VALUES (?,?,?,?)", (self.first_name, self.last_name, self.email, self.birth,))
 
-        cursor.execute("INSERT INTO person (first_name,last_name,email,birth) VALUES (?,?,?,?)", (self.first_name, self.last_name, self.birth, self.email,))
-        
+    def is_student(self):
+        pass
+
+    def is_teacher(self):
+        self.registration = input("Por favor, insira o *número de registro* do professor:\n")
+        person_id = cursor.execute("SELECT last_insert_rowid()").fetchall()
+        cursor.execute("INSERT INTO Teacher (person_id,registration) VALUES (?,?)", (person_id[0][0], self.registration,))
+
+
+def print_full_table(table):
+    cursor_head = db.execute(f'SELECT * FROM {table}') #cursos para pegar o cabecalho
+    print([description[0] for description in cursor_head.description]) #imprimo o cabecalho
+    print(cursor.execute(f'SELECT * FROM {table}').fetchall())
+
 if __name__=="__main__":
 
     os.chdir(sys.path[0]) #seleciono o caminho atual como padrão
-    address = os.path.join('files', 'universidade.db')
+    address = os.path.join('files', 'university.db')
 
     db = sqlite3.connect(address) #Cria o bd se não existir
     cursor = db.cursor() #Cria um cursor para fazer as operações com o banco de dados
     cursor.execute("PRAGMA foreign_keys = ON").fetchall()
 
+    ### SQL Tables ###
+
     #Person Table
-    person_db = """CREATE TABLE IF NOT EXISTS person (
+    person_table = """CREATE TABLE IF NOT EXISTS Person (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    first_name TEXT,
-    last_name TEXT,
-    email TEXT,
+    first_name VARCHAR2(255),
+    last_name VARCHAR2(255),
+    email VARCHAR2(255),
     birth DATE);
     """
-
-    #Teacher Table
-    teacher_db = """CREATE TABLE IF NOT EXISTS teacher (
-    id INTEGER NOT NULL PRIMARY KEY,
-    person_id INTEGER NOT NULL REFERENCES person(id),
+    #Student Table
+    student_table = """CREATE TABLE IF NOT EXISTS Student (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    person_id INTEGER NOT NULL REFERENCES Person(id),
     registration INTEGER);
     """
     #Teacher Table
-    cursor.execute(person_db) #Executa a tabela pessoa
-    cursor.execute(teacher_db) #Executa a tabela professores
+    teacher_table = """CREATE TABLE IF NOT EXISTS Teacher (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    person_id INTEGER NOT NULL REFERENCES Person(id),
+    registration INTEGER);
+    """
+    #Course Table
+    course_table = """CREATE TABLE IF NOT EXISTS Course (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR2(255),
+    duration VARCHAR2(255),
+    syllabus TEXT);
+    """
+    #Class Table
+    class_table = """CREATE TABLE IF NOT EXISTS Class (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    schedule VARCHAR2(255),
+    teacher_id INTEGER NOT NULL REFERENCES Teacher(id),
+    course_id INTEGER NOT NULL REFERENCES Course(id));
+    """
+    #Grade Table
+    grade_table = """CREATE TABLE IF NOT EXISTS Grade (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    value INTEGER,
+    student_id INTEGER NOT NULL REFERENCES Student(id),
+    teacher_id INTEGER NOT NULL REFERENCES Teacher(id),
+    class_id INTEGER NOT NULL REFERENCES Class(id));
+    """    
+    cursor.execute(person_table) #Executa a tabela pessoa
+    cursor.execute(teacher_table) #Executa a tabela professores
 
     while 1:
         print("***Sistema de Gerenciamento dos Dados da Universidade.***")
@@ -64,13 +105,17 @@ if __name__=="__main__":
                 resp = input("")
                 if resp == '1':
                     print(f"Resposta {resp}")
-                    pessoa = Pessoa()
-                    cursor_head = db.execute('select * from person') #cursos para pegar o cabecalho
-                    print([description[0] for description in cursor_head.description]) #imprimo o cabecalho
-                    print(cursor.execute('SELECT * FROM person').fetchall())
+                    person_obj = Person()
                     db.commit() #executa as mudanças no bd
+                    print(f"A pessoa {person_obj.first_name} {person_obj.last_name} foi cadastrada com sucesso.\n")                    
+                    print_full_table('Person')
                 elif resp == '2':
                     print(f"Resposta {resp}")
+                    person_obj = Person()
+                    person_obj.is_teacher()
+                    db.commit() #executa as mudanças no bd
+                    print(f"A pessoa {person_obj.first_name} {person_obj.last_name} foi cadastrada com sucesso.\n")
+                    print_full_table('Teacher')
                 elif resp == '3':
                     print(f"Resposta {resp}")
                 elif resp == '4':
@@ -84,13 +129,14 @@ if __name__=="__main__":
             print("Resposta 2")
             while 1:
                 print("O que deseja consultar?")
-                print("'1' - Alunos cadastradas.")
-                print("'2' - Professores cadastradas.")
+                print("'1' - Alunos cadastrados.")
+                print("'2' - Professores cadastrados.")
                 print("'3' - Cursos existentes.")
                 print("'4' - Média dos alunos.")
                 resp = input("")
                 if resp == '1':
                     print(f"Resposta {resp}")
+                    print_full_table('Person')
                 elif resp == '2':
                     print(f"Resposta {resp}")
                 elif resp == '3':
