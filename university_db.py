b6c0e60df1eb15b18037c67c35c9f88807b87998
Leonardo Rivera <leodrivera@gmail.com>
2021-03-03 10:10:19 -0300

    Finalização da classe 'Curso'

--- university_db.py
+++ university_db.py
@@ -36,11 +36,17 @@ class Student(Person):
         person_id = cursor.execute("SELECT last_insert_rowid() FROM Person").fetchall()
         cursor.execute("INSERT INTO Student (person_id,registration) VALUES (?,?)", (person_id[0][0], self.registration,))
 
-class Curso:
+class Course:
     def __init__(self):
-        pass
+        self.name = input("Por favor, insira o *nome* do Curso:\n")
 
-class Turma(Curso):
+        self.duration = input("Por favor, insira a *duração* do curso, em meses:\n")
+        self.duration = check.duration(self.duration)
+
+        self.syllabus = input("Por favor, insira a *ementa* do curso:\n")
+        cursor.execute("INSERT INTO Course (name,duration,syllabus) VALUES (?,?,?)", (self.name, self.duration, self.syllabus,))
+
+class Class(Course):
     def __init__(self):
         pass
 
@@ -169,40 +175,45 @@ if __name__=="__main__":
                     db.commit() #executa as mudanças no bd
                     print(f"A pessoa {student_obj.first_name} {student_obj.last_name} foi cadastrada com sucesso.\n")                    
                     print_full_table('Student','table')
+
                 elif resp == '2':
                     print(f"Resposta {resp}")
-                    person_obj = Person()
-                    person_obj.is_teacher() #Método para inserir os dados adicionais de professor
+                    teacher_obj = Teacher()
                     db.commit() #executa as mudanças no bd
-                    print(f"A pessoa {person_obj.first_name} {person_obj.last_name} foi cadastrada com sucesso.\n")
+                    print(f"A pessoa {teacher_obj.first_name} {teacher_obj.last_name} foi cadastrada com sucesso.\n")
                     print_full_table('Teacher','table')
+
                 elif resp == '3':
                     print(f"Resposta {resp}")
-                    name = input("Por favor, insira o *nome* do Curso:\n")
-                    duration = input("Por favor, insira a *duração* do curso, em meses:\n")
-                    syllabus = input("Por favor, insira a *ementa* do curso:\n")
-                    cursor.execute("INSERT INTO Course (name,duration,syllabus) VALUES (?,?,?)", (name, duration, syllabus,))
+                    course_obj = Course()
                     db.commit() #executa as mudanças no bd
-                    print(f"O curso {name} foi cadastrado com sucesso.\n")
+                    print(f"O curso {course_obj.name} foi cadastrado com sucesso.\n")
+                    print_full_table('Course','table')
+
                 elif resp == '4':
                     print(f"Resposta {resp}")
                     schedule = input("Por favor, insira o *horário* das aulas da turma:\n")
                     teacher_id = input("Por favor, insira o *id do professor* da turma:\n")
                     course_id = input("Por favor, insira o *id do do curso* relativo à turma:\n")
+
                     try:
                         cursor.execute("INSERT INTO Class (schedule,teacher_id,course_id) VALUES (?,?,?)", (schedule, teacher_id, course_id,))
                         db.commit() #executa as mudanças no bd
                         id_class = cursor.execute("SELECT last_insert_rowid()").fetchall()
                         print(f"A turma {id_class[0][0]} foi cadastrada com sucesso.\n")
+
                     except sqlite3.IntegrityError:
                         print("Verifique se você inseriu os ids corretamente.")
+
                 elif resp == '5':
                     print(f"Resposta {resp}")
                     student_id = input("Por favor, insira o *id do aluno*:\n")
                     class_id = input("Por favor, insira o *id da turma*:\n")
+
                     try:
                         course_id_atual = cursor.execute('SELECT Co.id FROM Grade G INNER JOIN Class Cl ON (G.student_id = ?) AND (G.class_id = ?) AND (G.class_id = Cl.id) '
                         'INNER JOIN Course Co ON (Co.id = Cl.course_id)',(student_id,class_id,)).fetchall()
+
                         if course_id_atual != []:
                             student_tries = cursor.execute('SELECT COUNT(G.student_id) FROM Grade G INNER JOIN Class Cl ON (G.student_id = ?) AND (G.class_id = Cl.id) '
                             'INNER JOIN Course Co ON (Cl.course_id = Co.id) AND (Co.id = ?)',(student_id,course_id_atual[0][0],)).fetchall()
@@ -211,9 +222,11 @@ if __name__=="__main__":
                             #'AND (G.class_id = Cl.id) AND (Co.id = Cl.course_id)'(student_id,class_id,)).fetchall()
                             #student_tries = cursor.execute('SELECT COUNT(G.student_id) FROM Grade G, Class Cl, Course Co WHERE (G.student_id = ?) AND (G.class_id = Cl.id) AND '
                             #'(Cl.course_id = Co.id) AND (Co.id = ?)',(student_id,course_id_atual[0][0],)).fetchall()
+
                             if student_tries[0][0] > 3:
                                 student_name = cursor.execute('SELECT P.first_name, P.last_name FROM Student S INNER JOIN Person P ON (S.person_id = P.id) AND (S.id = ?)',(student_id,)).fetchall()
                                 print(f"O aluno {student_name[0][0]} {student_name[0][1]} de id {student_id} já realizou o curso por 3 vezes.")
+
                         else:
                             value = input("Por favor, insira a *nota do aluno*:\n")
                             cursor.execute("INSERT INTO Grade (student_id,class_id,value) VALUES (?,?,?)", (student_id, class_id, value,))
@@ -221,13 +234,17 @@ if __name__=="__main__":
                             student_name = cursor.execute('SELECT P.first_name, P.last_name FROM Student S INNER JOIN Person P ON (S.person_id = P.id) '
                             'INNER JOIN Grade G ON (S.id = G.student_id) ORDER BY G.id DESC').fetchall()
                             print(f"O aluno {student_name[0][0]} {student_name[0][1]} teve sua nota cadastrada com sucesso.\n")
+
                     except NameError or sqlite3.IntegrityError:
                         print("ATENÇÃO! Verifique se você inseriu os ids corretamente.\n")
+
                 else:
                     print("Saindo do módulo cadastro.")
                     break
+
         elif resp == '2':
             print("Resposta 2")
+
             while 1:
                 print("O que deseja consultar?")
                 print("'1' - Alunos cadastrados.")
@@ -236,22 +253,27 @@ if __name__=="__main__":
                 print("'4' - Turmas existentes.")
                 print("'5' - Nota dos alunos.")
                 resp = input("")
+
                 if resp == '1':
                     print(f"Resposta {resp}")
                     sql_inner_join = ('SELECT S.id, P.first_name, P.last_name, P.email, P.birth, S.registration '
                     'FROM Person P INNER JOIN Student S ON P.id=S.person_id')
                     print_full_table(sql_inner_join,'join')
+
                 elif resp == '2':
                     print(f"Resposta {resp}")
                     sql_inner_join = ('SELECT T.id, P.first_name, P.last_name, P.email, P.birth, T.registration '
                     'FROM Person P INNER JOIN Teacher T ON P.id=T.person_id')
                     print_full_table(sql_inner_join,'join')
+
                 elif resp == '3':
                     print(f"Resposta {resp}")
                     print_full_table('Course','table')
+
                 elif resp == '4':
                     print(f"Resposta {resp}")
                     print_full_table('Class','table')
+
                 elif resp == '5':
                     print(f"Resposta {resp}")
                     print_full_table('Grade','table')
@@ -269,11 +291,13 @@ if __name__=="__main__":
                     'Grade, (CASE WHEN SUM(t1.value)/COUNT(t1.value) >= 70 THEN "APPROVED" ELSE "DISAPPROVED" END) as Result, t2.name as "Course Name" '
                     f'FROM ({sql_inner_join_student}) t1 INNER JOIN ({sql_join_teacher}) t2 ON (t1.class_id = t2.cl_id) GROUP BY t2.name')
                     print_full_table(sql_join_total_2,'join')
+
                 else:
                     print("Saindo do módulo consulta.")
                     break
         else:
             print("Saindo do programa.")
             break
+        
     db.close() #Fecha a conexão com o bd
             	
