7261e425d9b80b3be185008deac3f982b3137ac8
Leonardo Rivera <leodrivera@gmail.com>
2021-03-03 15:41:11 -0300

    Finalização da Classe turma

--- university_db.py
+++ university_db.py
@@ -13,42 +13,61 @@ class Person():
     def __init__(self):
         self.first_name = input("Por favor, insira o *primeiro nome* da pessoa:\n")
         self.last_name = input("Por favor, insira o *último nome* da pessoa:\n")
-
         self.birth = input("Por favor, insira a *data de nascimento* da pessoa, no formato dd/mm/yyyy:\n")
         self.birth = check.birth(self.birth) # função para checar se a data está no formato correto
-
         self.email = input("Por favor, insira o *e-mail* da pessoa:\n")
         self.email = check.email(self.email) # função para checar se a data está no formato correto
 
+    def table_insert(self):
         cursor.execute("INSERT INTO Person (first_name,last_name,email,birth) VALUES (?,?,?,?)", (self.first_name, self.last_name, self.email, self.birth,))
 
 class Teacher(Person):
     def __init__(self):
         super().__init__()
+        super().table_insert()
         self.registration = input("Por favor, insira o *número de registro* do professor (5 digitos):\n")
-        person_id = cursor.execute("SELECT last_insert_rowid() FROM Person").fetchall()
-        cursor.execute("INSERT INTO Teacher (person_id,registration) VALUES (?,?)", (person_id[0][0], self.registration,))
+        self.person_id = cursor.execute("SELECT last_insert_rowid() FROM Person").fetchall()
+
+    def table_insert(self):
+        cursor.execute("INSERT INTO Teacher (person_id,registration) VALUES (?,?)", (self.person_id[0][0], self.registration,))
 
 class Student(Person):
     def __init__(self):
         super().__init__()
+        super().table_insert()
         self.registration = input("Por favor, insira o *número de registro* do aluno (5 dígitos):\n")
-        person_id = cursor.execute("SELECT last_insert_rowid() FROM Person").fetchall()
-        cursor.execute("INSERT INTO Student (person_id,registration) VALUES (?,?)", (person_id[0][0], self.registration,))
+        self.person_id = cursor.execute("SELECT last_insert_rowid() FROM Person").fetchall()
+
+    def table_insert(self):
+        cursor.execute("INSERT INTO Student (person_id,registration) VALUES (?,?)", (self.person_id[0][0], self.registration,))
 
 class Course:
     def __init__(self):
         self.name = input("Por favor, insira o *nome* do Curso:\n")
-
         self.duration = input("Por favor, insira a *duração* do curso, em meses:\n")
         self.duration = check.duration(self.duration)
-
         self.syllabus = input("Por favor, insira a *ementa* do curso:\n")
+
+    def table_insert(self):
         cursor.execute("INSERT INTO Course (name,duration,syllabus) VALUES (?,?,?)", (self.name, self.duration, self.syllabus,))
 
 class Class(Course):
     def __init__(self):
-        pass
+        self.schedule = input("Por favor, insira o *horário* das aulas da turma:\n")
+        self.teacher_id = input("Por favor, insira o *id do professor* da turma:\n")
+        self.teacher_id = check.id(self.teacher_id)
+        self.course_id = input("Por favor, insira o *id do do curso* relativo à turma:\n")
+        self.course_id = check.id(self.course_id)
+
+    def table_insert(self):
+        try:
+            cursor.execute("INSERT INTO Class (schedule,teacher_id,course_id) VALUES (?,?,?)", (self.schedule, self.teacher_id, self.course_id,))
+            self.id_class = cursor.execute("SELECT last_insert_rowid() FROM Class").fetchall()  #O número da turma é o seu próprio ID
+            return True
+
+        except sqlite3.IntegrityError:
+            print("Verifique se você inseriu os ids corretamente e tente novamente\n.")
+            return False
 
 class Check:
     def __regex_check(self):
@@ -60,16 +79,15 @@ class Check:
 
     def registration(self, registration):
         self.text = registration
-        self.regex = '\d{5}' # para detectar 5 dígitos
+        self.regex = '^\d{5}$' # para detectar 5 dígitos
         self.message = "O *número de registro* deve conter 5 dígitos:\n"
         return self.__regex_check()
 
     def birth(self, date):
         self.text = date
-        self.regex = '^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}' # para detectar dd/mm/aaaa
+        self.regex = '^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$' # para detectar dd/mm/aaaa
         self.message = "A *data de nascimento* deve estar no formato dd/mm/yyyy:\n"
         return self.__regex_check()
-
 
     def email(self, email):
         self.text = email
@@ -80,8 +98,14 @@ class Check:
             
     def duration(self, duration):
         self.text = duration
-        self.regex = '\d+' # para detectar 3 dígitos
+        self.regex = '^\d+$' # para detectar dígitos
         self.message = "Por favor, insira somente o número de meses:\n"
+        return self.__regex_check()
+
+    def id(self, id):
+        self.text = id
+        self.regex = '^\d+$' # para detectar dígitos
+        self.message = "O *id* deve conter somente dígitos :\n"
         return self.__regex_check()
 
 def print_full_table(table, cond='table'):
@@ -172,6 +196,7 @@ if __name__=="__main__":
                 if resp == '1':
                     print(f"Resposta {resp}")
                     student_obj = Student()
+                    student_obj.table_insert()
                     db.commit() #executa as mudanças no bd
                     print(f"A pessoa {student_obj.first_name} {student_obj.last_name} foi cadastrada com sucesso.\n")                    
                     print_full_table('Student','table')
@@ -179,6 +204,7 @@ if __name__=="__main__":
                 elif resp == '2':
                     print(f"Resposta {resp}")
                     teacher_obj = Teacher()
+                    teacher_obj.table_insert()
                     db.commit() #executa as mudanças no bd
                     print(f"A pessoa {teacher_obj.first_name} {teacher_obj.last_name} foi cadastrada com sucesso.\n")
                     print_full_table('Teacher','table')
@@ -186,24 +212,19 @@ if __name__=="__main__":
                 elif resp == '3':
                     print(f"Resposta {resp}")
                     course_obj = Course()
+                    course_obj.table_insert()
                     db.commit() #executa as mudanças no bd
                     print(f"O curso {course_obj.name} foi cadastrado com sucesso.\n")
                     print_full_table('Course','table')
 
                 elif resp == '4':
                     print(f"Resposta {resp}")
-                    schedule = input("Por favor, insira o *horário* das aulas da turma:\n")
-                    teacher_id = input("Por favor, insira o *id do professor* da turma:\n")
-                    course_id = input("Por favor, insira o *id do do curso* relativo à turma:\n")
-
-                    try:
-                        cursor.execute("INSERT INTO Class (schedule,teacher_id,course_id) VALUES (?,?,?)", (schedule, teacher_id, course_id,))
+                    class_obj = Class()
+                    result = class_obj.table_insert()
+                    if result: #se tiver ok
                         db.commit() #executa as mudanças no bd
-                        id_class = cursor.execute("SELECT last_insert_rowid()").fetchall()
-                        print(f"A turma {id_class[0][0]} foi cadastrada com sucesso.\n")
-
-                    except sqlite3.IntegrityError:
-                        print("Verifique se você inseriu os ids corretamente.")
+                        print(f"A turma {class_obj.id_class[0][0]} foi cadastrada com sucesso.\n")
+                        print_full_table('Class','table')
 
                 elif resp == '5':
                     print(f"Resposta {resp}")
@@ -211,12 +232,14 @@ if __name__=="__main__":
                     class_id = input("Por favor, insira o *id da turma*:\n")
 
                     try:
+                        #Método para pegar o id do curso
                         course_id_atual = cursor.execute('SELECT Co.id FROM Grade G INNER JOIN Class Cl ON (G.student_id = ?) AND (G.class_id = ?) AND (G.class_id = Cl.id) '
                         'INNER JOIN Course Co ON (Co.id = Cl.course_id)',(student_id,class_id,)).fetchall()
 
                         if course_id_atual != []:
                             student_tries = cursor.execute('SELECT COUNT(G.student_id) FROM Grade G INNER JOIN Class Cl ON (G.student_id = ?) AND (G.class_id = Cl.id) '
                             'INNER JOIN Course Co ON (Cl.course_id = Co.id) AND (Co.id = ?)',(student_id,course_id_atual[0][0],)).fetchall()
+                            print(f"student_tries: {student_tries}")
                             ##Forma alternativa##
                             #course_id_atual = cursor.execute('SELECT Co.id FROM Grade G, Class Cl, Course Co WHERE (G.student_id = ?) AND (G.class_id = ?) ' 
                             #'AND (G.class_id = Cl.id) AND (Co.id = Cl.course_id)'(student_id,class_id,)).fetchall()
@@ -228,7 +251,7 @@ if __name__=="__main__":
                                 print(f"O aluno {student_name[0][0]} {student_name[0][1]} de id {student_id} já realizou o curso por 3 vezes.")
 
                         else:
-                            value = input("Por favor, insira a *nota do aluno*:\n")
+                            value = input("Por favor, insira a *nota do aluno*, variando de 0 a 100:\n")
                             cursor.execute("INSERT INTO Grade (student_id,class_id,value) VALUES (?,?,?)", (student_id, class_id, value,))
                             db.commit() #executa as mudanças no bd
                             student_name = cursor.execute('SELECT P.first_name, P.last_name FROM Student S INNER JOIN Person P ON (S.person_id = P.id) '
@@ -277,19 +300,24 @@ if __name__=="__main__":
                 elif resp == '5':
                     print(f"Resposta {resp}")
                     print_full_table('Grade','table')
-                    print('')                
-                    sql_inner_join_student = ('SELECT P.first_name, P.last_name, G.value, G.class_id FROM Student S INNER JOIN Person P ON (S.person_id = P.id) '
+                    print('')
+                    #Pego o estudante que consta em 'Person', 'Student' e 'Grade', sua nota e o ID da turma
+                    sql_inner_join_student = ('SELECT P.id, P.first_name, P.last_name, G.value, G.class_id FROM Student S INNER JOIN Person P ON (S.person_id = P.id) '
                     'INNER JOIN Grade G ON (S.id = G.student_id)')
+                    #Pego o professor que consta em 'Person', 'Teacher' e 'Classe' e o nome do curso
                     sql_join_teacher = ('SELECT P.first_name, P.Last_name, Co.name, Cl.id as cl_id FROM Teacher T, Person P, Course Co, Class Cl WHERE '
                     '(Cl.teacher_id = T.id) AND (P.id = T.person_id) AND (Co.id = Cl.course_id)')
+
                     sql_join_total_1 = ('SELECT t1.first_name as "Student First Name", t1.last_name as "Student Last Name", t1.value as Grade, '
                     f't2.first_name as "Teacher First Name", t2.Last_name as "Teacher Last Name", t2.name as "Course Name" FROM ({sql_inner_join_student}) t1 '
                     f'INNER JOIN ({sql_join_teacher}) t2 ON (t1.class_id = t2.cl_id)')
+                    print('### Nota dos alunos por turma ###')
                     print_full_table(sql_join_total_1,'join')
-                    print('')
+                    
                     sql_join_total_2 = ('SELECT t1.first_name as "Student First Name", t1.last_name as "Student Last Name", SUM(t1.value)/COUNT(t1.value) as '
                     'Grade, (CASE WHEN SUM(t1.value)/COUNT(t1.value) >= 70 THEN "APPROVED" ELSE "DISAPPROVED" END) as Result, t2.name as "Course Name" '
-                    f'FROM ({sql_inner_join_student}) t1 INNER JOIN ({sql_join_teacher}) t2 ON (t1.class_id = t2.cl_id) GROUP BY t2.name')
+                    f'FROM ({sql_inner_join_student}) t1 INNER JOIN ({sql_join_teacher}) t2 ON (t1.class_id = t2.cl_id) GROUP BY t1.id, t2.name')
+                    print('\n### Nota dos alunos consolidada ###')
                     print_full_table(sql_join_total_2,'join')
 
                 else:
